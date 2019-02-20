5bfd80c9bca8ec1557f4199a8be5fa4fd4dee1a1
AlyaSelviaTamzila <42310789+AlyaSelviaTamzila@users.noreply.github.com>
2019-02-20 14:07:03 +0700

    Add files via upload

---
+++ Latihan.php
@@ -0,0 +1,13 @@
+<?php
+$nama = "Alyast";
+$sekolah = "SMK Telkom Malang";
+$umur = "16";
+
+//menampilkan niai variabel
+//contoh 1
+echo "Halo namaku $nama usiaku $umur tahun";
+echo "<br>";
+echo "Aku sekolah di $sekolah";
+echo "<br>";
+
+?>
---
+++ Latihan1.php
@@ -0,0 +1,5 @@
+<?php
+echo "Namaku Alyast";
+echo "<br>";
+echo "Aku sedang belajar PHP di SMK Telkom Malang";
+?>
